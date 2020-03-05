0984d408ead4f1dfdf2f1a27c00d96e22af52e7a
frankx13 <frankx13580@gmail.com>
2020-03-05 21:32:15 +0100

    - Minor fix on the WeightManager navigation

--- app/src/main/java/com/studio/neopanda/healthinbox/WeightManagerActivity.kt
+++ app/src/main/java/com/studio/neopanda/healthinbox/WeightManagerActivity.kt
@@ -1,16 +1,18 @@
 package com.studio.neopanda.healthinbox
 
-import android.content.Intent
 import android.os.Bundle
 import com.studio.neopanda.healthinbox.base.BaseToolbarActivity
 import kotlinx.android.synthetic.main.activity_weight_manager.*
 
 class WeightManagerActivity : BaseToolbarActivity() {
+
+    private val targetActivity = MenuActivity()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weight_manager)
 
-        configureToolbar(toolbar_weight_manager, this)
+        configureToolbar(toolbar_weight_manager, targetActivity)
     }
 
     override fun getLayoutContentViewID(): Int {
